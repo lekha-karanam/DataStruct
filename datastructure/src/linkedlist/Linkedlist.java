8eaa2f483d8b7739d830b7b4586283cd348dc2e6
lekha-karanam <lekha.prakash1@gmail.com>
2021-09-17 16:38:36 +0530

    Initial commit

---
+++ datastructure/src/linkedlist/Linkedlist.java
@@ -0,0 +1,35 @@
+package linkedlist;
+
+public class Linkedlist {
+	Node head = null;
+	public void insert(int data) {
+		Node n = new Node();
+		n.data= data;
+		n.next= null;
+		if (head==null) {
+			head=n;
+		}
+		else {
+			Node n1 = head;
+			while(n1.next!=null) {
+				n1=n1.next;
+			}
+			n1.next=n;
+		}
+	}
+	public void delete(int index) {
+		
+	}
+
+
+	public void show() {
+		Node n=head;
+		while(n.next!=null) {
+			System.out.println(n.data+" ");
+			n=n.next;
+		}
+		System.out.println(n.data+" ");
+	}
+	
+
+}
---
+++ datastructure/src/linkedlist/Runner.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Linkedlist l=new Linkedlist();
+		l.insert(10);
+		l.insert(20);
+		l.show();
+
+	}
+
+}
